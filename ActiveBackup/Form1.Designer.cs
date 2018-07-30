224c9f33d698c08a620166de3c70ca0f9d9794a1
LaszloPoroszkai <laszlo.poroszkai@gmail.com>
2018-07-30 15:08:48 +0200

    ActiveBackup initial progress

---
+++ ActiveBackup/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace ActiveBackup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 94);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(323, 173);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(431, 91);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(303, 173);
+            this.listBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(431, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(303, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(341, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(341, 146);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Remove";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(760, 94);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Add";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(760, 146);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Remove";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(760, 29);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Change";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Folders to watch";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(428, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Files to skip";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(338, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Backup location:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(341, 207);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Apply";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(760, 207);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "Apply";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 29);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(102, 23);
+            this.startButton.TabIndex = 13;
+            this.startButton.Text = "Start Watcher";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(167, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Disk Full:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(223, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(112, 20);
+            this.textBox2.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 283);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ActiveBackup/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace ActiveBackup
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog ofd = new OpenFileDialog();
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadSettings();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.Add(fbd.SelectedPath);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<String> paths = new List<String>();
+            foreach (String path in listBox1.Items)
+            {
+                paths.Add(path);
+            }
+            if (SaveContent("checkedfolders.txt", paths))
+            {
+                MessageBox.Show("Modifications saved!", "Message", MessageBoxButtons.OK);
+            }
+            else
+            {
+                MessageBox.Show("An error occured. Saving failed!", "Error", MessageBoxButtons.OK);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                listBox2.Items.Add(ofd.FileName);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.RemoveAt(listBox2.SelectedIndex);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            List<String> whitelisted = new List<String>();
+            foreach (String path in listBox2.Items)
+            {
+                whitelisted.Add(path);
+            }
+            if (SaveContent("filestokeep.txt", whitelisted))
+            {
+                MessageBox.Show("Modifications saved!", "Message", MessageBoxButtons.OK);
+            }
+            else
+            {
+                MessageBox.Show("An error occured. Saving failed!", "Error", MessageBoxButtons.OK);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = fbd.SelectedPath;
+            }
+            if (Directory.Exists(textBox1.Text))
+            {
+                List<String> path = new List<string>();
+                path.Add(textBox1.Text);
+                Environment.SetEnvironmentVariable("backuppath", textBox1.Text);
+                SaveContent("backuplocation.txt", path);
+            }
+        }
+
+        private bool SaveContent(String fileName, List<String> paths)
+        {
+            try
+            {
+                if (File.Exists(fileName))
+                {
+                    File.Delete(fileName);
+                }
+                System.IO.File.WriteAllLines(fileName, paths);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        private void LoadSettings()
+        {
+            if (File.Exists("checkedfolders.txt"))
+            {
+                foreach (String line in File.ReadAllLines("checkedfolders.txt"))
+                {
+                    listBox1.Items.Add(line);
+                }
+            }
+            if (File.Exists("filestokeep.txt"))
+            {
+                foreach (String line in File.ReadAllLines("filestokeep.txt"))
+                {
+                    listBox2.Items.Add(line);
+                }
+            }
+            if (File.Exists("backuplocation.txt"))
+            {
+                textBox1.Text = File.ReadAllLines("backuplocation.txt")[0];
+                Environment.SetEnvironmentVariable("backuppath", textBox1.Text);
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (Environment.GetEnvironmentVariable("backuppath") != null && listBox1.Items.Count > 0)
+            {
+                Thread watchThread = new Thread(new ThreadStart(this.WatchThread));
+                watchThread.Start();
+            }
+            else
+            {
+                MessageBox.Show("Backup location or folders to watch not configured!", "Error", MessageBoxButtons.OK);
+            }
+        }
+
+        private void WatchThread()
+        {
+            while (true)
+            {
+                TimeSpan timespan = new TimeSpan(0, 0, 10);
+                DriveInfo kDrive = new DriveInfo("K");
+                setPercentage(((kDrive.AvailableFreeSpace / (float)kDrive.TotalSize) * 100).ToString());
+                if (Convert.ToDouble(textBox2.Text) < 25)
+                {
+                    StartBackup();
+                }
+                Thread.Sleep(timespan);
+            }
+        }
+
+        delegate void setPercentageCallback(string text);
+
+        private void setPercentage(string text)
+        {
+            if (this.textBox2.InvokeRequired)
+            {
+                setPercentageCallback x = new setPercentageCallback(setPercentage);
+                this.Invoke(x, new object[] { text });
+            }
+            else
+            {
+                this.textBox2.Text = text;
+            }
+        }
+
+        private void StartBackup()
+        {
+            foreach(String path in listBox1.Items)
+            {
+                foreach(String file in Directory.GetFiles(path))
+                {
+                    if (!listBox2.Items.Contains(file))
+                    {
+                        File.SetAttributes(file, FileAttributes.Normal);
+                        String fileName = Path.GetFileName(file);
+                        File.Move(file, Environment.GetEnvironmentVariable("backuppath") + @"\" +  fileName);
+                    }
+                }
+            }
+        }
+    }
+}
+
